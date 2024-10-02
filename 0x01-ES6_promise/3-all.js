9a3b43a52e082fe76a84efc4f515fd366bcb9f32
amagyei <kwame.na@hotmail.com>
2024-10-02 12:59:22 +0000

    task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import {uploadPhoto, createUser} from './utils.js'
+
+export default function handleProfileSignup() {
+    return Promise
+        .all([uploadPhoto(), createUser()])
+        .then((res) => {
+            console.log(`${res[0].body} ${res[1].firstName} ${res[2].lastname}`)
+        })
+        .catch(() => {console.log('Signup system offline')});
+    
+}
