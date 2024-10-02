2de82871572cbe66fd72b09a87f130e276523fed
amagyei <kwame.na@hotmail.com>
2024-10-02 13:04:33 +0000

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,10 @@
-import {uploadPhoto, createUser} from './utils.js'
+import { uploadPhoto, createUser } from './utils.js'
 
 export default function handleProfileSignup() {
     return Promise
         .all([uploadPhoto(), createUser()])
         .then((res) => {
-            console.log(`${res[0].body} ${res[1].firstName} ${res[2].lastname}`)
+            console.log(`${res[0].body} ${res[1].firstName} ${res[2].lastName}`)
         })
         .catch(() => {console.log('Signup system offline')});
     
