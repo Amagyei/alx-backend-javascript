44ee70c224c96590bba8601fa66ffc8f4c587546
amagyei <kwame.na@hotmail.com>
2024-10-04 00:33:40 +0000

    task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,13 @@
+export default class HolbertonCourse {
+    constructor(name, length, students) {
+        if(typeof name === 'string'){
+            this._name = name
+        }
+        if(typeof length === 'number'){
+            this._length = length
+        }
+        if(typeof students === 'object'){
+            this._students = students
+        }
+    }
+}
