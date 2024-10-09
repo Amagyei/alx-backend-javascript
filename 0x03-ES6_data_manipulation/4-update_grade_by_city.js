5a9868316126ec0d15f8951307ef7c1b3c0ab17b
amagyei <kwame.na@hotmail.com>
2024-10-09 03:41:36 +0000

    task 4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,16 @@
+export default function updateStudentGradeByCity(arr, city, newGrades){
+  const defaultGrade = { grade: 'N/A' };
+  if (!Array.isArray(arr)) {
+    return [];
+  }
+  return arr
+    .filter((student) => student.location === city)
+    .map((student) => ({
+      id: student.id,
+      firstName: student.firstName,
+      location: student.location,
+      grade:(newGrades
+        .filter((grade) => grade.studentId ===student.id)
+        .pop() || defaultGrade).grade,
+    }));
+}
