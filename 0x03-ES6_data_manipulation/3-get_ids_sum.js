53ab64470ab7ab3ef8bb5bedc17b06697e8790be
amagyei <kwame.na@hotmail.com>
2024-10-09 03:21:36 +0000

    taskk 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -2,5 +2,5 @@ export default function getStudentIdsSum(arr) {
   if (!Array.isArray(arr)) {
     return 0;
   }
-  return arr.reduce((sum, {id}), => sum + id, 0)
+  return arr.reduce((sum, {id}) => sum + id, 0)
 }
