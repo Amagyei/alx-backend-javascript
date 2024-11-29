7933c6684796f7734f6cfd50fcb028139ab8a722
amagyei <kwame.na@hotmail.com>
2024-11-29 00:46:26 +0000

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('readable', () => {
-    chunk = process.stdin.read();
 
-    if (chunk) {
-        process.stdout.write(`Your name is: ${chunk}`);
-        }
-    })
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
+});
 
-process.stdin.on('end', () =>{
-    process.stdout.write('This important software is now closing\n')
-    })
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
